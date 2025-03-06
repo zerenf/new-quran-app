66bc8acbb07c9a1739110d0736eff7dff5391641
zerenf <zerenfirat7@gmail.com>
2025-03-06 14:13:14 +0300

    header title padding

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,10 +10,10 @@ export default function RootLayout({ children }) {
 		<html>
 			<body className="vsc-initialized">
 				<div className="header-container">
-					<div style={{ position: "absolute", left: 10, top: 10 }}>
-						<img src="/quran-logo.png" alt="" style={{ width: 40 }} />
+					<div className="logo-container">
+						<img src="/quran-logo.png" alt="Quran Logo" className="logo" />
 					</div>
-					<div style={{ flex: 1 }}>
+					<div className="header-title-container">
 						<h1 className="header-text">
 							كِتَابٌ أَنزَلْنَاهُ إِلَيْكَ مُبَارَكٌ لِّيَدَّبَّرُوا آيَاتِهِ وَلِيَتَذَكَّرَ أُولُو الْأَلْبَابِ
 						</h1>
