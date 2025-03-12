93d2fe3b9603774d28188dc40a6a7019e7445ceb
zerenf <zerenfirat7@gmail.com>
2025-03-12 17:56:22 +0300

    header

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css"
+import Header from "./Header"
 
 export const metadata = {
 	title: "Kur'an'ın Meali",
@@ -9,20 +10,7 @@ export default function RootLayout({ children }) {
 	return (
 		<html>
 			<body className="vsc-initialized">
-				<div className="header-container">
-					<div className="logo-container">
-						<img src="/quran-logo.png" alt="Quran Logo" className="logo" />
-					</div>
-					<div className="header-title-container">
-						<p className="header-text">
-							كِتَابٌ أَنزَلْنَاهُ إِلَيْكَ مُبَارَكٌ لِّيَدَّبَّرُوا آيَاتِهِ وَلِيَتَذَكَّرَ أُولُو الْأَلْبَابِ
-						</p>
-
-						<p className="header-text-meal">
-							Sana bu mübarek Kitab'ı, ayetlerini derin derin düşünsünler ve aklı olanlar öğüt alsınlar diye indirdik. (38/29)
-						</p>
-					</div>
-				</div>
+				<Header />
 				{children}
 			</body>
 		</html>
